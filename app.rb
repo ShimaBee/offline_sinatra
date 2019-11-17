7f888b2636bd13352368586b4f75986b0e751273
島袋雄太 <mrs1122ani@gmail.com>
2019-11-17 12:07:36 +0900

    signup function

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pg'
+
+db = PG::connect(:dbname => "offline_sinatra_app")
 
 get '/' do
   erb :index
@@ -7,6 +10,14 @@ end
 
 get '/signup' do
   erb :signup
+end
+
+post '/signup' do
+  name = params[:name]
+  password = params[:password]
+  # binding.irb
+  db.exec("insert into users (name, password) values($1, $2)",[name, password])
+  redirect '/login'
 end
 
 get '/login' do
